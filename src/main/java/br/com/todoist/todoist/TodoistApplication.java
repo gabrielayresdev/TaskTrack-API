41f580f50b66cc995215d6442d9990f7b36503cc
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-10-11 19:20:11 -0300

    initial commit

---
+++ src/main/java/br/com/todoist/todoist/TodoistApplication.java
@@ -0,0 +1,12 @@
+package br.com.todoist.todoist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoistApplication.class, args);
+	}
+}
---
+++ src/main/java/br/com/todoist/todoist/user/UserModel.java
@@ -0,0 +1,7 @@
+package br.com.todoist.todoist.user;
+
+public class UserModel {
+    String username;
+    String name; 
+    String password;
+}
