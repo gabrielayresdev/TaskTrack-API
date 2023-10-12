18b133f593bea87963760e486e48cd37566d7d17
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-10-12 04:51:42 -0300

    fix: bug where name was not being printed

--- src/main/java/br/com/todoist/todoist/user/UserModel.java
+++ src/main/java/br/com/todoist/todoist/user/UserModel.java
@@ -1,7 +1,7 @@
 package br.com.todoist.todoist.user;
 
 public class UserModel {
-    String username;
-    String name; 
-    String password;
+    public String username;
+    public String name; 
+    public String password;
 }
