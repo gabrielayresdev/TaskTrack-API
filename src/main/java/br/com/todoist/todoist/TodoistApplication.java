5ff03272fbf1ff42312867e76282665c51fc82dc
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-10-14 05:17:25 -0300

    feat: User Authentication. Task register method created

--- src/main/java/br/com/todoist/todoist/TodoistApplication.java
+++ src/main/java/br/com/todoist/todoist/TodoistApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodoistApplication {
 
+	/// /usr/bin/env C:\\Program\ Files\\Java\\jdk-17\\bin\\java.exe @C:\\Users\\USURIO~2\\AppData\\Local\\Temp\\cp_9judyd7u9nlu1azgdwa037ajf.argfile br.com.todoist.todoist.TodoistApplication 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoistApplication.class, args);
 	}
