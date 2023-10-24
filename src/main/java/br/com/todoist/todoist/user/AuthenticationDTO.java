05b5a9296eeeb5fc7f26a0f5845b97dc7d041d27
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-10-23 21:30:10 -0300

    refactor: removed old project

--- src/main/java/br/com/todoist/todoist/user/AuthenticationDTO.java
+++
@@ -1,5 +0,0 @@
-package br.com.todoist.todoist.user;
-
-public record AuthenticationDTO(String username, String password) {
-    
-}
--- src/main/java/br/com/todoist/todoist/user/LoginResponseDTO.java
+++
@@ -1,5 +0,0 @@
-package br.com.todoist.todoist.user;
-
-public record LoginResponseDTO(String token) {
-    
-}
