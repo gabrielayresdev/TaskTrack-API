cfb298f0d99bc4d42b9b6a6f5a142d3ec33cb4da
Gabriel Ayres <gabriel.ar.fort@gmail.com>
2023-10-27 22:32:52 -0300

    refactor: all configurations files moved to infra.security package

--- src/main/java/dev/gabrielayres/Todolist/infra/security/WebMvcConfig.java
+++ src/main/java/dev/gabrielayres/Todolist/infra/security/WebMvcConfig.java
@@ -1,4 +1,4 @@
-package dev.gabrielayres.Todolist.config;
+package dev.gabrielayres.Todolist.infra.security;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
